23a290f82cc17d315e4796d2e84048ca431bb683
nikolai <144839823+unitnikolai@users.noreply.github.com>
2025-02-16 17:56:34 -0500

    Add files via upload

--- index.py
+++ index.py
@@ -6,31 +6,82 @@ from curl_cffi import requests, Curl, CurlOpt
 import re
 from curl_cffi.requests import AsyncSession
 import asyncio
+import time
+import csv
+
+
+class dataResults:
+    def __init__(self, propertyData, moreLinks):
+        self.data = propertyData
+        self.links = moreLinks
 
 def zipcodeUrls():
     zipCodesOhio = []
     urls = []
     low = int(input("Lower bound Zipcode: "))
-    high = int(input("Higher bound Zipcode: "))
+    high = int(input("Higher bound Zipcode: ")) + 1
+    sub_urls = []
+    zipCodeCity = 'zipcodesUSA.csv'
+     
+    with open(zipCodeCity, 'r') as file:
+        read = csv.reader(file)
+        for col in read:
+            zip_code = int(col[0])
+            for x in range (low, high):
+                if zip_code == x:
+                    city = str(col[1]).lower()
+                    state = str(col[2]).lower()
+                    subUrl = city + "-" + state
+                    sub_urls.append(subUrl)
+        
     for n in range (low, high):
         code = str(n)
         zipCodesOhio.append(code)
     for code in zipCodesOhio:
-        URL = "https:/zillow.com/homes/" + code
-        urls.append(URL)
+        urlOne = "https://zillow.com/homes/" + code
+        urlTwo = "https://zillow.com/home/for_sale/" + code 
+        urls.append(urlOne)
+        urls.append(urlTwo)
+    
+    for x in sub_urls:
+        end = str(x)
+        urlThree = "https://zillow.com/homes/" + end
+        urlFour = "https://zillow.com/homes/for_sale/" + end
+        urls.append(urlThree)
+        urls.append(urlFour)
+    print(urls) 
     return urls
 
 
 async def main():
     queryZip = zipcodeUrls()
     urls = await getEstates(queryZip)
-    print(urls)
+    newUrls = []
+    for uL in urls:
+        if uL not in newUrls:
+            newUrls.append(uL)
+    #print(newUrls)
     print("Scraper started...")
-    data = await scrape_prop(urls)
-    print(data)
+    time.sleep(3)    
+    data = await scrape_prop(newUrls)
+    print(data) 
+    db = 'ohiodb.csv'
+    
+    existing_items = set()
 
+    #with open(db, mode='r', newline='') as file:
+    #    reader = csv.reader(file)
+    #    for row in reader:
+    #        existing_items.add(tuple(row))
+    
+    #items_add = [item for item in data if tuple(row) not in existing_items]
 
-#Collect links of postings on each homelisting page
+    #if items_add:
+    with open(db, 'a') as file:
+        entry = csv.writer(file)
+        #for item in items_add:
+            #entry.writerow(item)
+        entry.writerow(data)
 
 async def getEstates(zipLinks):
     url_results = []
@@ -40,7 +91,10 @@ async def getEstates(zipLinks):
     zillowPattern = r'href="[^"]*"'
     link_pattern = r'href="([^"]+)"'    
     async with AsyncSession() as client:
-        scrape = [client.get(z, impersonate = "safari_ios") for z in zipLinks]
+        for z in zipLinks:
+            siteData = client.get(z, impersonate = "safari_ios")
+            scrape.append(siteData)
+            time.sleep(0.2)
         responses = await asyncio.gather(*scrape)
         for response in responses:
             if response.status_code != 200:
@@ -59,11 +113,13 @@ async def getEstates(zipLinks):
                             if hrefUrl.startswith('href="https://www.zillow.com/homedetails'):
                                 hrefUrl = re.search(r'href="([^"]+)"', hrefUrl).group(1)
                                 links.append(hrefUrl)
-            return links
+        return links
 
 async def scrape_prop(urls):
     results = []
     scrapeSites = []
+    linksSite = []
+    links = []
     async with AsyncSession() as client:
         for url in urls:
             site = client.get(url, impersonate = "safari_ios")
@@ -71,7 +127,6 @@ async def scrape_prop(urls):
         responses = await asyncio.gather(*scrapeSites)
         print("Fetching data...")
         for response in responses:
-            print(response)
             if response.status_code != 200:
                 print("ERROR: ", response.status_code, "request has been blocked")
             else:
@@ -116,6 +171,19 @@ async def scrape_prop(urls):
                         price = entry['price']
                         salesHistory.append(date)
                         salesHistory.append(price)
+                    
+                    zillowPattern = r'href="[^"]*"'
+                    newUrls = parse.find_all('a')
+                    linksSite.append(newUrls)
+                    for i in linksSite:
+                        for x in i:
+                            elmnt = str(x)
+                            tags = re.findall(zillowPattern, elmnt)
+                            for a in tags:
+                                hrefUrl = str(a)
+                                if hrefUrl.startswith('href="https://www.zillow.com/homedetails'):
+                                    hrefUrl = re.search(r'href="([^"]+)"', hrefUrl).group(1)
+                                    links.append(hrefUrl)
 
                 else:
 
@@ -138,9 +206,13 @@ async def scrape_prop(urls):
             prop.append(address)
             prop.append(details)
             prop.append(salesHistory)
+            prop.append(url)
             results.append(prop)
-
+        data = dataResults(results, links)
+    
     return results
+
+
 
 
 asyncio.run(main())
